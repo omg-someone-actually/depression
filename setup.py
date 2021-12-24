52168d4ccf514bb5ee5c2e7e1e7c606a06768a53
Eddie <58668105+omg-someone-actually@users.noreply.github.com>
2021-12-24 14:54:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Eddie V',                   # Type in your name
   author_email = 'eddieblevins015@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/omg-someone-actually/depression',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/omg-someone-actually/depression/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['DEPRESSION', 'MEANINGFULL', 'LIFE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
