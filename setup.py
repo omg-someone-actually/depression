a91d4e8e4da57a8af16e3f699ee28ffb95e35d47
Eddie <58668105+omg-someone-actually@users.noreply.github.com>
2021-12-24 14:55:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,7 @@ setup(
   url = 'https://github.com/omg-someone-actually/depression',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/omg-someone-actually/depression/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['DEPRESSION', 'MEANINGFULL', 'LIFE'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
