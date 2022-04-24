4962e0f6eb705271317286a6f08283ff14f4bae2
Eddie <58668105+omg-someone-actually@users.noreply.github.com>
2022-04-24 13:14:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'depression',         # How you named your package folder (MyLib)
   packages = ['depression'],   # Chose the same as "name"
-  version = '2.0',      # Start with a small number and increase it with every change you make
+  version = '2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'depression',   # Give a short description about your library
   author = 'Eddie B',                   # Type in your name
   author_email = 'eddieblevins015@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/omg-someone-actually/depression',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/omg-someone-actually/depression/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/omg-someone-actually/depression/archive/refs/tags/2.1.tar.gz',    # I explain this later on
   keywords = ['DEPRESSION', 'MEANINGFULL', 'LIFE'],   # Keywords that define your package best
   install_requires=[
     'time',
